4d15d8d204be70d47a8ad0df5747c82c43947c8f
Marcus Michaels <hello@modestindustries.co>
2020-02-25 13:20:37 +0000

    Generated build folder for use in gh-pages

---
+++ build/precache-manifest.107d791df06a1f7a3a744e6c1bdfe547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8543494a7fbc7c1addad5289c38f04a3",
+    "url": "/markdown-generator/index.html"
+  },
+  {
+    "revision": "aefa872979c60d5d07a9",
+    "url": "/markdown-generator/static/css/main.28aba5a6.chunk.css"
+  },
+  {
+    "revision": "927209602c6a28c26a65",
+    "url": "/markdown-generator/static/js/2.2d5404f0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/markdown-generator/static/js/2.2d5404f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefa872979c60d5d07a9",
+    "url": "/markdown-generator/static/js/main.e6ef5d4c.chunk.js"
+  },
+  {
+    "revision": "b055a6a291c528c29eb2",
+    "url": "/markdown-generator/static/js/runtime-main.08424359.js"
+  }
+]);
